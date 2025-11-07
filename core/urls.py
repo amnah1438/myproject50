158c5d7fb4036c1efcf789918234d23cb0643ddb
amnah1438 <amnah1438@gmail.com>
2025-11-07 15:40:57 +0300

    تعديل الريايز  في الكور

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),                  # الصفحة الرئيسية
+    path('', views.home, name='home'),              # الصفحة الرئيسية
     path('register/', views.register_view, name='register'),  # إنشاء حساب
-    path('login/', views.login_view, name='login'),          # تسجيل الدخول
-    path('logout/', views.logout_view, name='logout'),        # تسجيل الخروج
+    path('login/', views.login_view, name='login'),           # تسجيل دخول
+    path('logout/', views.logout_view, name='logout'),        # تسجيل خروج
 ]
