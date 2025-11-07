a25e3a9bb6f255bf50e463173bf82555541cceb7
amnah1438 <amnah1438@gmail.com>
2025-11-07 12:49:50 +0300

    تعديل اليورلس في الكور

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views  # ✅ استدعاء ملف العرض (views.py)
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # الصفحة الرئيسية
+    path('', views.home, name='home'),                  # الصفحة الرئيسية
+    path('register/', views.register_view, name='register'),  # إنشاء حساب
+    path('login/', views.login_view, name='login'),          # تسجيل الدخول
+    path('logout/', views.logout_view, name='logout'),        # تسجيل الخروج
 ]
-
