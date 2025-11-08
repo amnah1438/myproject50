ece560978b659f83c945bb394693f2268596d068
amnah1438 <amnah1438@gmail.com>
2025-11-08 14:37:38 +0300

    تعديل ريلز

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "store"  # 🏷️ لتجنب التعارض بين المسارات
+app_name = "store"
 
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
